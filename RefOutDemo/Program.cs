49ecd12984beb8ac30e60abbe9e326a5254b673a
priyanka <priyankajadhav@gmail.com>
2025-04-09 07:27:11 +0530

    Ref and Out parameters example in C#

---
+++ RefOutDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Two number");
+        int num1 = int.Parse(Console.ReadLine()) ;
+         int num2 = int.Parse(Console.ReadLine()) ;
+        int sub, add;
+
+        Result(ref num1,ref num2,out sub ,out add);
+        Console.WriteLine($"Addition is {add} and Sunstaction is {sub} of {num1} and {num2}");
+
+    }
+    static void Result( ref int num1,  ref int  num2, out int sub,out int add){
+        sub= num1-num2;
+        add=num1+num2 ;
+
+    }
+
+}
